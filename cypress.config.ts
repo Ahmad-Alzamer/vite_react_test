8967524ab1104aa55fe304b4f6c0ef05651126da
Ahmad Alzamer <ahmed.alzamer@gmail.com>
2023-12-26 13:56:23 -0500

    tried enabling component testing but cypress is not supporting vite ^5.0.0 at the moment, so put it on hold and either look into a fix or downgrade vite to ^4.0.0

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from "cypress";
-import vitePreprocessor from 'cypress-vite'
+import vitePreprocessor from "cypress-vite";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173/',
+    baseUrl: "http://localhost:5173/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('file:preprocessor', vitePreprocessor())
+      on("file:preprocessor", vitePreprocessor());
     },
   },
+  //
+  // component: {
+  //   devServer: {
+  //     framework: "react",
+  //     bundler: "vite",
+  //   },
+  // },
 });
